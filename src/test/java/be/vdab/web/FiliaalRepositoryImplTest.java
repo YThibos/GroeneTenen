a3c4d74fa1223d3431f6169a67b5add5706a44d1
Yannick Thibos <yannick.thibos@gmail.com>
2016-05-30 15:22:23 +0200

    Integration testing added

---
+++ src/test/java/be/vdab/web/FiliaalRepositoryImplTest.java
@@ -0,0 +1,38 @@
+package be.vdab.web;
+
+import static org.junit.Assert.assertNotEquals;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import be.vdab.entities.Filiaal;
+import be.vdab.repositories.FiliaalRepository;
+import be.vdab.repositories.RepositoriesConfig;
+import be.vdab.valueobjects.Adres;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes= {TestDataSourceConfig.class, RepositoriesConfig.class})
+@Transactional
+public class FiliaalRepositoryImplTest {
+	
+	@Autowired
+	private FiliaalRepository filiaalRepository;
+	
+	@Test
+	public void create() {
+		Filiaal filiaal = new Filiaal("Testnaam", true, BigDecimal.ONE, LocalDate.now(), new Adres("straat", "huisnr", 0, "gemeente"));
+		filiaalRepository.create(filiaal);
+		
+		// ID wordt ingevuld na het aanmaken in de database
+		// -> mag dus niet 0 zijn (autonumber)
+		assertNotEquals(0, filiaal.getId());
+	}
+
+}
