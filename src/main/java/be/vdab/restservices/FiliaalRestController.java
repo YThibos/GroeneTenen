8d1df3e207758f71c8c3f3f6373ef65b56341596
Yannick Thibos <yannick.thibos@gmail.com>
2016-06-07 16:57:44 +0200

    POST request added to create filiaal via REST-service

--- src/main/java/be/vdab/restservices/FiliaalRestController.java
+++ src/main/java/be/vdab/restservices/FiliaalRestController.java
@@ -1,9 +1,14 @@
 package be.vdab.restservices;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -30,6 +35,31 @@ class FiliaalRestController {
 			throw new FiliaalNietGevondenException();
 		}
 		return filiaal;
+	}
+	
+	@RequestMapping(path = "{filiaal}", method = RequestMethod.DELETE)
+	void delete(@PathVariable Filiaal filiaal) {
+		if (filiaal == null) {
+			throw new FiliaalNietGevondenException();
+		}
+		filiaalService.delete(filiaal.getId());
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	void create(@RequestBody @Valid Filiaal filiaal) {
+		filiaalService.create(filiaal);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	String filiaalMetVerkeerdeProperties(MethodArgumentNotValidException ex) {
+		StringBuilder fouten = new StringBuilder();
+		for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+			fouten.append(error.getField()).append(':')
+			.append(error.getDefaultMessage()).append('\n');
+		}
+		fouten.deleteCharAt(fouten.length() - 1);
+		return fouten.toString();
 	}
 	
 	@ExceptionHandler(FiliaalNietGevondenException.class)
